932ec7b804cea9ab64f623f76e23bf790122b6b6
Matthew Saber <mattsaber21@yahoo.com>
2021-04-01 15:22:21 -0700

    simplify

--- Product/agentData.js
+++ Product/agentData.js
@@ -1,74 +1,76 @@
 export const agents = [
 
-    //tier1
 
     {
+        id: 1,
         agent: 'sloth',
         tier: 1,
         description: 'Its adorable, and illegal to own in most states. calling animal control is a sure way to give your pesky x roommate an uncomfortable conversation with local law enforcement.',
-        price: '$75/day',
+        price: 75,
     },
-
     {
+        id: 2,
         agent: 'parrot',
         tier: 1,
         description: 'This feathered repot expert can argue with the best of them. Polly McCraker was raised in a ship yard where peir workers and construction personele swapped many colorful insults between them.',
-        price: '$125/day',
+        price: 125,
     },
 
     {
+        id: 3,
         agent: 'llama',
         tier: 1,
         description: 'Few individuals admit to both loving a fearing this towering spit slinger. We\'ve recieved reports of this repo expert chewing pajamas down by the bay, actively stubbing taget\'s toes and generally mucifiying the area. We are not responsible if the items in question belong to the target or are those being repossed.',
-        price: '$150/day',
+        price: 150,
     },
 
-    //tier2
-
     {
+        id: 4,
         agent: 'alligator',
         tier: 1,
         description: 'This modern dino has lots of teeth and a bad attitude. It makes quarterly reviews challenging, but what Alice lacks in curticy she more than makes up for in professional acumen.',
-        price: '$200/day',
+        price: 200,
     },
 
     {
+        id: 5,
         agent: 'baboon',
         tier: 2,
         description: 'Terrible house guest. No they wont be getting their security deposit back after this one.',
-        price: '$250/day',
+        price: 250,
     },
 
     {
+        id: 6,
         agent: 'elephant',
         tier: 2,
         description: 'Too big to do anything with. Impossible to ignore. Sure to make the safe return of your items a top priority',
-        price: '$300/day',
+        price: 300,
     },
 
     //tier3
 
     {
+        id: 7,
         agent: 'jaguar',
         tier: 3,
         description: 'Yes those documentries you saw were true. If you\'d like an update of your repo\'s progress, just tune in to the new\'s interview of your x roomies\' neighborhood lock down.',
-        price: '$350/day',
+        price: 350,
     },
 
     {
+        id: 8,
         agent: 'komodoDragon',
         tier: 3,
         description: 'Komo is fast, hungry and built like a tank. Sure to bring even the toughest negotiator to the table',
-        price: '$400/day',
+        price: 400,
     },
 
     {
+        id: 9,
         agent: 'tiger',
         tier: 3,
         description: 'Tiega is reserved for federal cases only. Once we have the required documents and releases on file we can discuss their services more in depth.',
-        price: '$: Undisclossed'
+        price: 9000
     }
 ];
-//To Do
-
-createAgentsLi;
