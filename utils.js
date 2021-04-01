34b7b8ca66e4b3fb2db8d662412158e2cd55ffc5
Matthew Saber <mattsaber21@yahoo.com>
2021-04-01 16:25:02 -0700

    create agent li

--- utils.js
+++ utils.js
@@ -5,3 +5,22 @@ export function findById(agents, id) {
         }
     }
 }
+export function createAgentLi(someAgent) {
+    const li = document.createElement('li');
+    li.classList.add('styleLi');
+
+    const pName = document.createElement('p');
+    pName.textContent = someAgent.agent;
+
+    const pTier = document.createElement('p');
+    pTier.textContent = someAgent.tier;
+
+    const pBio = document.createElement('p');
+    pBio.textContent = someAgent.description;
+
+    const pPrice = document.createElement('p');
+    pPrice.textContent = `Price: $${someAgent.price}/day`;
+
+    li.append(pName, pTier, pBio, pPrice);
+    return li;
+}
