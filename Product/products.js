ac1cf9462352371c13067da66bb2653add996a97
Matthew Saber <mattsaber21@yahoo.com>
2021-04-01 15:56:24 -0700

    created for loop for lis

--- Product/products.js
+++ Product/products.js
@@ -1,1 +1,10 @@
 //for Loop to render lis
+import { agents } from './agentData.js';
+import { createAgentLi } from '../utils.js';
+
+const ul = document.querySelector('#list');
+
+for (let agent of agents) {
+    const li = createAgentLi(agent);
+    ul.append(li);
+}
